fe1acb2ec08374f19a2654ebf420aad07230fbf0
CodePulse <99908796+Codepulze@users.noreply.github.com>
2023-06-30 09:48:39 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,24 @@
-import random
 import string
+import secrets
+
+def evaluate_password_strength(password):
+    strength = 0
+
+    if any(char.islower() for char in password):
+        strength += 1
+    if any(char.isupper() for char in password):
+        strength += 1
+    if any(char.isdigit() for char in password):
+        strength += 1
+    if any(char in string.punctuation for char in password):
+        strength += 1
+
+    if strength <= 2:
+        return 'Weak'
+    elif strength == 3:
+        return 'Medium'
+    else:
+        return 'Strong'
 
 def generate_password(length=12, include_lowercase=True, include_uppercase=True, include_digits=True, include_symbols=True):
     lowercase_letters = string.ascii_lowercase if include_lowercase else ''
@@ -9,7 +28,7 @@ def generate_password(length=12, include_lowercase=True, include_uppercase=True,
     
     characters = lowercase_letters + uppercase_letters + digits + symbols
     
-    password = ''.join(random.choice(characters) for _ in range(length))
+    password = ''.join(secrets.choice(characters) for _ in range(length))
     return password
 
 length = int(input("Enter the desired password length: "))
@@ -18,9 +37,10 @@ include_uppercase = input("Include uppercase letters? (y/n): ").lower() == 'y'
 include_digits = input("Include digits? (y/n): ").lower() == 'y'
 include_symbols = input("Include symbols? (y/n): ").lower() == 'y'
 
-# Validate if at least one character set is selected
 if not (include_lowercase or include_uppercase or include_digits or include_symbols):
     print("You cannot choose nothing as a password.")
 else:
     password = generate_password(length, include_lowercase, include_uppercase, include_digits, include_symbols)
+    strength = evaluate_password_strength(password)
     print("Generated Password:", password)
+    print("Password Strength:", strength)
